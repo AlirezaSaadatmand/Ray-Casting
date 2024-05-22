3093b34cf9c2a2d7d4429f948c184e23bdbfb6d4
Alireza <alirezasaadat056@gmail.com>
2024-05-22 08:52:55 -0700

    casting rays

--- rayCasting.py
+++ rayCasting.py
@@ -1,13 +1,15 @@
 import pygame
 from sys import exit
 import random
+import math
 
-WIDTH , HEIGHT = 1000 , 600
+WIDTH , HEIGHT = 1200 , 600
 
 boundary_lst = []
 
 particle_lst = []
 
+ray_lst = []
 class Particle:
     def __init__(self , x , y):
         self.x = x
@@ -17,6 +19,27 @@ class Particle:
     def show(self):
         pygame.draw.circle(screen, "white" , (self.x , self.y), 10)
         
+class Ray:
+    def __init__(self , x , y , ix , iy):
+        self.x = x
+        self.y = y
+        self.ix = ix
+        self.iy = iy
+        self.start = (self.x , self.y)
+        self.dir = (self.x + self.ix*10 , self.y + self.iy*10)
+    
+    def update(self ,x , y):
+        self.start = (x , y)
+        self.dir = (x + self.ix*100 , y + self.iy*100)
+
+
+        
+    def show(self):
+        pygame.draw.line(screen , "white" , self.start , self.dir)
+        
+        
+for i in range(0 , 360 , 1):
+    ray_lst.append(Ray(WIDTH/ 2 , HEIGHT/2 , math.cos(math.radians(i)) , math.sin(math.radians(i))))
 class Boundary:
     def __init__(self , x1 , y1 , x2 , y2):
         self.a = (x1 , y1)
@@ -30,12 +53,14 @@ for _ in range(5):
         
 def draw():
     pos = pygame.mouse.get_pos()
-    for b in boundary_lst:
-        b.draw()
-    for particle in particle_lst:
-        particle.update(pos)
-        particle.show()
-        
+    # for b in boundary_lst:
+    #     b.draw()
+    # for particle in particle_lst:
+    #     particle.update(pos)
+    #     particle.show()
+    for ray in ray_lst:
+        ray.update(pos[0] , pos[1])
+        ray.show()
 pygame.init()
 screen = pygame.display.set_mode( (WIDTH , HEIGHT) ) 
 pygame.display.set_caption("Ray Casting")
@@ -53,4 +78,4 @@ while True:
         particle_lst.append(Particle(*pos))
     draw()
     pygame.display.update()
-    clock.tick()
+    clock.tick(60)
